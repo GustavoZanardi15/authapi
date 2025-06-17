8be9210aa5bd2a18958b083abad719f8587e4099
GustavoZanardi15 <ulianzanardig@gmail.com>
2025-06-17 07:19:21 -0300

    :rocket: Estrutura inicial do projeto Spring Boot com dependências básicas

---
+++ src/test/java/com/jwtapp/authapi/AuthapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jwtapp.authapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
